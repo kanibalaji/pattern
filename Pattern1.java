24e8e4d927b66951e02735188801a7c4371f1acd
unknown <kanibalaji16@gmail.com>
2023-06-25 14:48:03 +0530

    initial commit

---
+++ Pattern1.java
@@ -0,0 +1,24 @@
+package org.patternprogram;
+import java.util.Scanner;
+public class Pattern1 {
+	public static void main(String[] arg) {
+		System.out.println("Enter no of times patterns to be printed ");
+		
+	Scanner sc= new Scanner(System.in);
+	int n=sc.nextInt();
+	for(int i=1;i<=n;i++)
+	{
+		System.out.println();
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print(j);
+		}
+		
+		}
+			
+		}
+	}
+	
+	
+
+
